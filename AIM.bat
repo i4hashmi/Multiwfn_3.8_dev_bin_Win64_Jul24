2d7821b6d6840ab8a420cb4bc51913136c13906e
Dr. Muhammad Ali Hashmi <i4hashmi@hotmail.com>
2025-09-29 09:22:40 +1300

    ‘add’

---
+++ AIM.bat
@@ -0,0 +1,2 @@
+Multiwfn emim_I-C4_ethup_St_Cl_b3lypg3bj6311gdp_lanl2dz_freqR.fchk < AIM.txt
+move /Y *.pdb "D:\Multiwfn_3.8_dev_bin_Win64_Jul24\VMD"
---
+++ ESPext.bat
@@ -0,0 +1,5 @@
+Multiwfn 1.fch < ESPext.txt
+move /Y vtx.pdb vtx1.pdb
+move /Y mol.pdb mol1.pdb
+
+move /Y *.pdb "D:\Multiwfn_3.8_dev_bin_Win64_Jul24\VMD"
